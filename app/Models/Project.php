0b678e065b709a333493e7d17c54f8291ec4c0d5
Frankosviz <geom.francescopaolo@gmail.com>
2024-05-30 17:02:13 +0200

    added slug/column to db - migration/add method well done

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'slug',
+        'technologies_used',
+        'start_date',
+        'end_date',
+        'status',
+        'url',
+        'repository_url',
+        'image_path',
+    ];
+
 }
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\ComicController;
-
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,7 +22,7 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-    //Route::resource('comics', ComicController::class);
+    Route::resource('projects', ProjectController::class);
 });
 
 

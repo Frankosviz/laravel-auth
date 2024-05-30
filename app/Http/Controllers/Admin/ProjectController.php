8b5c6d3bf6e2b0293a37f7d5194556ee81d662da
Frankosviz <geom.francescopaolo@gmail.com>
2024-05-30 17:42:53 +0200

    going for the print

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
-
 use App\Models\Project;
 use Illuminate\Http\Request;
 
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Project extends Model
 {
@@ -21,5 +22,10 @@ class Project extends Model
         'repository_url',
         'image_path',
     ];
+
+    public static function generateSlug($title)
+    {
+        return Str::slug($title, '-');
+    }
 
 }

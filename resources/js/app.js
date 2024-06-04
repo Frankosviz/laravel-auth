940a9c7c5c61c4b233b98c897a401fdddf84dc62
Frankosviz <geom.francescopaolo@gmail.com>
2024-06-04 22:35:39 +0200

    let the user show the img-preview : done

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,9 +14,25 @@ document.getElementById('search-icon').addEventListener('click', function() {
     }
 });
 
+// prendo la casella di input 
 const image = document.getElementById('upload_image');
+
+// se esiste nella pagina 
 if (image) {
     image.addEventListener('change', () => {
+
+        // prendo l'elemento img dove voglio la preview
         const preview = document.getElementById('uploadPreview');
+
+        // creo nuovo oggetto file reader
+        const oFReader = new FileReader();
+
+        // uso il metodo readAsDataURL per leggere il file dell'immagine
+        oFReader.readAsDataURL(image.files[0]);
+        // al termine della lettura del file
+        oFReader.onload = function(event){
+            // metto nel src della preview l'immagine
+            preview.src = event.target.result;
+        }
     });
 }
